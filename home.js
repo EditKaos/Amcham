98a1cff16120288de6bdbe9349456f9795c19ca0
unknown <editkaos.design@gmail.com>
2021-03-22 01:13:38 -0600

    amcham menu

--- home.js
+++ home.js
@@ -29,3 +29,33 @@ function showSlides(n) {
   dots[slideIndex - 1].className += " active";
 }
 
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
+const navSlide = ()=> {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const close = document.querySelector('.close-icon');
+  
+  burger.addEventListener('click', ()=> {
+    nav.classList.add('nav-active');
+    close.classList.add('close-icon-active'); 
+  });
+  
+  close.addEventListener('click', ()=>{
+    close.classList.remove('close-icon-active');
+    nav.classList.remove('nav-active');
+  })
+}
+
+navSlide();
+
+
+
